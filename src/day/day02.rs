f482915e92faa01f9dfc98ad8793b3579d715707
samkaj <kajavasamuel@gmail.com>
2024-12-02 13:50:52 +0100

    [4/50]

--- src/day/day02.rs
+++ src/day/day02.rs
@@ -16,8 +16,26 @@ fn part1(lines: Vec<String>) -> String {
     safe_count.to_string()
 }
 
-fn part2(_lines: Vec<String>) -> String {
-    "".to_string()
+fn part2(lines: Vec<String>) -> String {
+    let mut safe_count = 0;
+
+    for line in lines {
+        let levels = list_from_line(line);
+        let mut i = 0;
+        while i < levels.len() {
+            let mut subset = levels.clone();
+            subset.remove(i);
+
+            if level_is_safe(subset) {
+                safe_count += 1;
+                break;
+            }
+
+            i += 1;
+        }
+    }
+
+    safe_count.to_string()
 }
 
 fn list_from_line(line: String) -> Vec<i32> {
@@ -42,6 +60,21 @@ fn all_decreasing(list: Vec<i32>) -> bool {
     orig.sort();
     orig.reverse();
     orig == list
+}
+
+fn number_of_errors(list: Vec<i32>) -> i32 {
+    let mut iter = list.iter();
+    let mut prev = iter.next().expect("empty list");
+    let mut count = 0;
+    for n in iter {
+        let diff = i32::abs(n - prev);
+        if diff == 0 || diff > 3 {
+            count += 1;
+        }
+        prev = n;
+    }
+
+    count
 }
 
 fn diff_ok(list: Vec<i32>) -> bool {
@@ -77,8 +110,13 @@ mod tests {
 
     #[test]
     fn test_solve_part2() {
-        let test_input = r#""#;
+        let test_input = r#"7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9"#;
         let lines = util::multiline_to_vec(test_input);
-        assert_eq!(part2(lines), "11");
+        assert_eq!(part2(lines), "4");
     }
 }
